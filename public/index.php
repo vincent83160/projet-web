159e9beda36abb1015022de67c0b4197f02aba08
vincent83160 <gary.vincent3107@gmail.com>
2024-02-29 14:29:56 +0100

    test inex.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-require("../model/user.php"); 
+require("../model/user.php");
 session_start();
 
 $requestUri = $_SERVER['REQUEST_URI'];
@@ -7,66 +7,70 @@ $requestUri = $_SERVER['REQUEST_URI'];
 
 
 // Vérifier si l'utilisateur est connecté 
-$requestUri = trim($_SERVER['REQUEST_URI'],'/');
+$requestUri = filter_var(trim($_SERVER['REQUEST_URI'], '/'), FILTER_SANITIZE_URL);
 $segments = explode('/', $requestUri);
 // Récupérer le dernier élément du tableau
 
 
-$controller =$segments[0]."Controller"  ;
-if(isset($segments[1])){
+$controller = $segments[0]  . "Controller";
+if (isset($segments[1])) {
     $methode = $segments[1];
 }
-  
-$nbSegments = count($segments);
-// Vérifier si l'utilisateur est connecté   
 
+$nbSegments = count($segments);
+
+
+if ($nbSegments >= 3) {
+    $params = array();
+    for ($i = 2; $i < $nbSegments; $i++) {
+        $params[$segments[$i]] = $_POST[$segments[$i]];
+    }
+}
 
 if (isset($_SESSION['login'])) {
-    // Routes pour les utilisateurs connectés 
-    // $controller = '/controller/' . $controller; 
-    switch ($nbSegments-1) {
 
-        
-        case 1:
-            require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/' . $controller .".php";
+    switch ($nbSegments) {
+
+        case 2:
+            require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/' . $controller . ".php";
             $controller =  new $controller();
             $controller->$methode();
 
             break;
-        case 2:
-            require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/' . $controller .".php";
+        case $nbSegments >=3:
+            
+            require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/' . $controller . ".php";
+            $controller =  new $controller();
+            $controller->$methode($params);
             break;
 
         default:
-
             require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/DefaultController.php';
             $controller =  new DefaultController();
             $controller->accueil();
             break;
     }
-} else { 
+} else {
     switch ($nbSegments) {
         case 1:
-            require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/' . $controller .".php";
+            require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/' . $controller . ".php";
             break;
         case 2:
-            require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/' . $controller .".php"; 
+            require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/' . $controller . ".php";
             $controller =  new $controller();
             $controller->$methode();
 
             break;
         case 3:
-            require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/' . $controller .".php";
+            require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/' . $controller . ".php";
             $controller =  new $controller();
             $controller->$methode();
             break;
 
         default:
-            require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/UserController.php';
-            $controller =  new UserController();
-            $controller->login();
+            require_once $_SERVER["DOCUMENT_ROOT"] . '/controller/DefaultController.php';
+            $controller =  new DefaultController();
+            $controller->accueil();
             break;
- 
     }
 }
-
