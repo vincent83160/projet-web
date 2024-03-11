aa62575bc49ce06f11f496fedd77bcb5004e404b
vincent83160 <gary.vincent3107@gmail.com>
2024-03-11 15:19:08 +0100

    crud user

--- model/user.php
+++ model/user.php
@@ -1,40 +1,52 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . '/model/ConnexionMySql.php';
-class User extends ConnexionMySql{
-   private static $data = array();
+class User extends ConnexionMySql
+{
+
+    private static $data = array();
     private int $id;
-    private string $email; 
+    private string $email;
     private string $pseudo;
     private string $password;
     private bool $is_verified;
     private String $role;
 
+
+ 
+
     //GETTER
-    function getId(){
+    function getId()
+    {
         return $this->id;
     }
-    function getEmail(){
+    function getEmail()
+    {
         return $this->email;
     }
-    function getPseudo(){
+    function getPseudo()
+    {
         return $this->pseudo;
     }
-    function getPassword(){
+    function getPassword()
+    {
         return $this->password;
     }
-    function getIsVerified(){
+    function getIsVerified()
+    {
         return $this->is_verified;
     }
-    function getRole(){
+    function getRole()
+    {
         return $this->role;
     }
     //SETTER
-    public function __set($name, $value) {
-        $this->$name= $value;
-        
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
     }
     //CONSTRUCTEUR
-    function __construct($id, $email, $pseudo, $password, $is_verified, $role){
+    function __construct($id, $email, $pseudo, $password, $is_verified, $role)
+    {
         $this->id = $id;
         $this->email = $email;
         $this->pseudo = $pseudo;
@@ -45,5 +57,84 @@ class User extends ConnexionMySql{
         $this->password = $password;
         $this->is_verified = $is_verified;
         $this->role = $role;
+    }
+
+    public static function getConnexion()
+    { 
+        $db = new ConnexionMySql();
+        $db->connexion();
+        $pdo = $db->getPdo();
+        
+        return $pdo;  
+    }
+
+    public static function createVide()
+    {
+        return new self(0, "", "", "", false, "");
+    }
+
+ 
+public function update($input, $value,$idUser)
+    {
+        $pdo = $this->getConnexion();
+        $req = "UPDATE user SET " . $input . " = :value WHERE id = :idUser";
+        $stmt = $pdo->prepare($req); 
+        $stmt->bindParam(':value', $value);
+        $stmt->bindParam(':idUser', $idUser); 
+        $stmt->debugDumpParams();
+        $stmt->execute();
+    }
+
+public function create($email, $pseudo, $password, $is_verified, $role)
+    {
+        
+        $pdo = $this->getConnexion();
+        $req = "INSERT INTO user (email, pseudo, password, is_verified, role) VALUES (:email, :pseudo, :password, :is_verified, :role)";
+        $stmt = $pdo->prepare($req);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':pseudo', $pseudo);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':is_verified', $is_verified);
+        $stmt->bindParam(':role', $role);
+        $stmt->execute();
+    }
+
+
+
+    public function deleteUserByID($id)
+    {
+        $pdo = $this->getConnexion();
+        $req = 'DELETE FROM user WHERE id = :id';
+        $stmt = $pdo->prepare($req);
+        $stmt->bindParam(':id', $id); 
+        $result = $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    public function getUserByID($id)
+    {
+        $pdo = $this->getConnexion();
+        $req = 'SELECT * FROM user WHERE id = :id';
+        $stmt = $pdo->prepare($req);
+        $stmt->bindParam(':id', $id); 
+        $result = $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    public function checkLogin($login, $password)
+    {
+        $pdo = $this->getConnexion();
+        $req = 'SELECT * FROM user WHERE pseudo = :login AND password = :password';
+        $stmt = $pdo->prepare($req);
+        $stmt->bindParam(':login', $login);
+        $stmt->bindParam(':password', $password);
+        $result = $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result;
     }
 }
