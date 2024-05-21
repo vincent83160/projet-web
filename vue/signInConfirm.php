da2dd8365e2e35fa9686cc5cee637430db5f3fd7
AlexD972 <alexandreduclos83@gmail.com>
2024-05-21 09:32:37 +0200

    Ajout du design de la page de confirmation de connexion

--- vue/signInConfirm.php
+++ vue/signInConfirm.php
@@ -1,5 +1,25 @@
-<div >
-    <h1>Félicitation, vous etes inscrit</h1>
-    <p>Vous allez bientot recevoir un mail pour confirmer votre compte</p>
-    <a href=/Default/accueil>OK</a>
-</div>
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . '/vue/base.html';
+?>
+
+<head>
+   <title>Mon compte</title>
+   <link rel="stylesheet" href="/public/assets/css/account.css">
+   <link rel="icon" href="/public/assets/img/logo2.png" />
+</head>
+
+<body>
+   <div id="container-div-centrale">
+      <div id="div-centrale">
+         <div class="div-logo">
+            <img src="/public/assets/img/logo2.png" alt="Plein la bobine !">
+         </div>
+         <div >
+            <h1>Félicitation, vous etes inscrit</h1>
+            <p>Vous allez bientot recevoir un mail pour confirmer votre compte</p>
+            <div id="div-button">
+                <a href=/Default/accueil class="btn btn-primary">OK</a>
+            </div>    
+        </div>
+      </div>
+</body>
