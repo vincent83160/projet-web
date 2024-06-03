4d99a864d05a9bce2133f30f48487017117b494b
vincent83160 <gary.vincent3107@gmail.com>
2024-06-03 08:40:41 +0200

    .

--- controller/GameController.php
+++ controller/GameController.php
@@ -3,6 +3,10 @@
 class GameController {
 //fonction pour afficher la page de jeu
     function game() {
+        if(!isset($_SESSION['user']) || $_SESSION['user'] == null) {
+            header('Location: /login');
+            
+        }
         require_once $_SERVER["DOCUMENT_ROOT"] . "/controller/FilmController.php";
         $filmController = new FilmController(); 
         $filmController->checkIfFilmToFindToday();
